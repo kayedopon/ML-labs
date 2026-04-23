43055c7fc5913f1be3a28961277113f274fcb555
Kirill Postrehan <themrshayzoom882@gmail.com>
2026-04-23 20:14:48 +0300

    Change the name of transforms.py

---
+++ lb9/preprocessing.py
@@ -0,0 +1,47 @@
+from torchvision import transforms
+
+def get_resNet18_transforms():
+    mean=[0.485, 0.456, 0.406]
+    std=[0.229, 0.224, 0.225]
+
+    train_transforms = transforms.Compose([
+        transforms.Resize(256),
+        transforms.RandomResizedCrop(224),
+        transforms.TrivialAugmentWide(
+            interpolation=transforms.InterpolationMode.BILINEAR
+        ),
+        transforms.Normalize(mean, std),
+        transforms.ToTensor(),
+    ])
+
+    test_transforms = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.Normalize(mean, std),
+        transforms.ToTensor(),
+    ])
+
+    return train_transforms, test_transforms
+
+def get_resNet50_transforms():
+    mean=[0.485, 0.456, 0.406]
+    std=[0.229, 0.224, 0.225]
+
+    train_transforms = transforms.Compose([
+        transforms.Resize(232),
+        transforms.RandomResizedCrop(224),
+        transforms.TrivialAugmentWide(
+            interpolation=transforms.InterpolationMode.BILINEAR
+        ),
+        transforms.Normalize(mean, std),
+        transforms.ToTensor(),
+    ])
+
+    test_transforms = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.Normalize(mean, std),
+        transforms.ToTensor(),
+    ])
+
+    return train_transforms, test_transforms
