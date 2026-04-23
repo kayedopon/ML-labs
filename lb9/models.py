1a56a910fd339c6425de2575810ab6fbec3a0372
Kirill Postrehan <themrshayzoom882@gmail.com>
2026-04-23 20:18:44 +0300

    Create a file for transforms and remove them from model.py

--- lb9/models.py
+++ lb9/models.py
@@ -13,29 +13,6 @@ def get_resNet18(num_cls:int):
     resnet.fc = nn.Linear(resnet.fc.in_features, out_features=num_cls,)
     return resnet
 
-def get_resNet19_transforms():
-    mean=[0.485, 0.456, 0.406]
-    std=[0.229, 0.224, 0.225]
-
-    train_transforms = transforms.Compose([
-        transforms.Resize(256),
-        transforms.RandomResizedCrop(224),
-        transforms.TrivialAugmentWide(
-            interpolation=transforms.InterpolationMode.BILINEAR
-        ),
-        transforms.Normalize(mean, std),
-        transforms.ToTensor(),
-    ])
-
-    test_transforms = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.Normalize(mean, std),
-        transforms.ToTensor(),
-    ])
-
-    return train_transforms, test_transforms
-
 def get_resNet50(num_cls:int):
 
     weights = models.ResNet50_Weights.DEFAULT
@@ -45,3 +22,4 @@ def get_resNet50(num_cls:int):
         param.requires_grad = False
 
     resnet.fc = nn.Linear(in_features=resnet.fc.in_features, out_features=num_cls)
+    return resnet
