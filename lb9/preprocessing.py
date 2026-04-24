b0e6af929b5a399f144761a0ce65b6a4585bcb8d
Kirill Postrehan <themrshayzoom882@gmail.com>
2026-04-24 20:20:56 +0300

    Add transforms for effnet_b0

--- lb9/preprocessing.py
+++ lb9/preprocessing.py
@@ -5,7 +5,6 @@ def get_resNet18_transforms():
     std=[0.229, 0.224, 0.225]
 
     train_transforms = transforms.Compose([
-        transforms.Resize(256),
         transforms.RandomResizedCrop(224),
         transforms.TrivialAugmentWide(
             interpolation=transforms.InterpolationMode.BILINEAR
@@ -28,10 +27,31 @@ def get_resNet50_transforms():
     std=[0.229, 0.224, 0.225]
 
     train_transforms = transforms.Compose([
-        transforms.Resize(232),
         transforms.RandomResizedCrop(224),
         transforms.TrivialAugmentWide(
             interpolation=transforms.InterpolationMode.BILINEAR
+        ),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std),
+    ])
+
+    test_transforms = transforms.Compose([
+        transforms.Resize(232),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std),
+    ])
+
+    return train_transforms, test_transforms
+
+def get_effnet_b0_transforms():
+    mean=[0.485, 0.456, 0.406]
+    std=[0.229, 0.224, 0.225]
+
+    train_transforms = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.TrivialAugmentWide(
+            interpolation=transforms.InterpolationMode.BICUBIC
         ),
         transforms.ToTensor(),
         transforms.Normalize(mean, std),
